7035192fa90e5adbdd2100365edb72a68b8e9174
edwinson <moramillanjuandiego@gmail.com>
2025-07-04 16:43:26 -0500

    Experimento Creacion De Uusuarios
    Se crearon nuevos archivos, para la creación de los usuiario por el rol y para el avance de que se envie el usuario y contraseña por correo

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -1,17 +1,18 @@
 <?php
 namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
-use Illuminate\Database\Console\Seeds\WithoutModeLEvents;
-
-
 
 class RolePermissionSeeder extends Seeder
 {
     public function run()
     {
-       $adminRole = Role::create(['name' => 'admin']);
-        $colabRole = Role::create(['name' => 'colaborador']);
-        $instRole = Role::create(['name' => 'instructor']);
+        $roles = ['admin', 'colaborador', 'instructor'];
+
+        foreach ($roles as $role) {
+            Role::firstOrCreate(['name' => $role]);
+        }
     }
 }
+
