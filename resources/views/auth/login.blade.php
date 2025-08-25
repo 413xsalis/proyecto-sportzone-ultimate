afe506613b7bb2a63bf38e4fa66fbe6be24a1a6f
JUANdi28208 <moramillanjuandiego@gmail.com>
2025-08-25 02:09:41 -0500

    Update
    Se hizo la ruta para que enviara a la pagina de olvidar contraseña y se implemento una animación en el login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-xl-10">
-                    <div class="card rounded-3 text-black">
+                    <div class="card rounded-3 text-black animated-card ">
                         <div class="row g-0">
                             <div class="col-lg-6">
                                 <div class="card-body p-md-5 mx-md-4">
@@ -70,7 +70,7 @@
                                                 class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
                                                 type="submit">Login</button><br>
 
-                                            <a class="text-muted" href="#!">¿Olvidaste tu contraseña?</a>
+                                            <a class="text-muted" href="{{ route('password.request') }}">¿Olvidaste tu contraseña?</a>
                                         </div>
 
                                         <div class="d-flex align-items-center justify-content-center pb-4">
