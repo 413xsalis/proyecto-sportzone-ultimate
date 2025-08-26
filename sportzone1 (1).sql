3a9ab364a1dcdb0feb7aa33943eb66ebe504c47b
alex sanchez <alexsanchez.az2005@gmail.com>
2025-08-25 23:52:38 -0500

    actualizacionsql

--- sportzone1 (1).sql
+++ sportzone1 (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-08-2025 a las 15:45:28
+-- Tiempo de generación: 26-08-2025 a las 06:51:28
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -48,34 +48,11 @@ CREATE TABLE `cache_locks` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `contactos`
---
-
-CREATE TABLE `contactos` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `telefono` varchar(255) DEFAULT NULL,
-  `mensaje` text NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Volcado de datos para la tabla `contactos`
---
-
-INSERT INTO `contactos` (`id`, `nombre`, `email`, `telefono`, `mensaje`, `created_at`, `updated_at`) VALUES
-(1, 'Darío Barrios', 'dario@email.com', '100200300', 'petición de una cotización.', '2025-08-18 07:28:57', '2025-08-18 07:28:57');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `estudiantes`
 --
 
 CREATE TABLE `estudiantes` (
-  `documento` bigint(20) UNSIGNED NOT NULL,
+  `documento` int(11) NOT NULL,
   `nombre_1` varchar(255) NOT NULL,
   `nombre_2` varchar(255) DEFAULT NULL,
   `apellido_1` varchar(255) NOT NULL,
@@ -94,9 +71,7 @@ CREATE TABLE `estudiantes` (
 --
 
 INSERT INTO `estudiantes` (`documento`, `nombre_1`, `nombre_2`, `apellido_1`, `apellido_2`, `telefono`, `nombre_contacto`, `telefono_contacto`, `eps`, `id_grupo_nivel`, `created_at`, `updated_at`) VALUES
-(10000, 'Darío', 'Darío', 'Peraz', 'perez', '300100200', 'Sañora Perez', '300200100', 'Sanitas', '1', '2025-08-18 18:05:14', '2025-08-18 18:05:14'),
-(500600700, 'Camila', 'Andrea', 'Villa', 'Villa', '311900900', 'Sra. Villa', '311700700', 'Sura', '3', '2025-08-24 19:38:34', '2025-08-24 19:38:34'),
-(700700700, 'Rafael', 'Ricardo', 'Pinzon', 'Mayorga', '3174800399', 'señora Mayorga', '200200200', 'Nueva EPS', '2', '2025-08-18 19:36:22', '2025-08-18 19:36:22');
+(1073599572, 'Rafaelito', 'no aplica', 'veloza', 'gutierrez', '31748003993', 'qraweraw', '3001002001', 'compensar123', '3', '2025-08-26 09:43:00', '2025-08-26 09:43:00');
 
 -- --------------------------------------------------------
 
@@ -145,7 +120,8 @@ INSERT INTO `grupos` (`id`, `nombre`, `created_at`, `updated_at`) VALUES
 
 CREATE TABLE `horarios` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `fecha` varchar(255) NOT NULL,
+  `dia` varchar(255) NOT NULL,
+  `fecha` date NOT NULL,
   `hora_inicio` time NOT NULL,
   `hora_fin` time NOT NULL,
   `instructor_id` bigint(20) UNSIGNED NOT NULL,
@@ -158,36 +134,8 @@ CREATE TABLE `horarios` (
 -- Volcado de datos para la tabla `horarios`
 --
 
-INSERT INTO `horarios` (`id`, `fecha`, `hora_inicio`, `hora_fin`, `instructor_id`, `grupo_id`, `created_at`, `updated_at`) VALUES
-(2, '2025-08-24', '08:00:00', '09:00:00', 2, 2, '2025-08-24 06:40:32', '2025-08-24 06:40:32'),
-(4, '2025-08-29', '12:30:00', '14:30:00', 3, 2, '2025-08-24 06:43:58', '2025-08-24 18:34:25'),
-(5, '2025-08-30', '14:30:00', '17:30:00', 1, 1, '2025-08-24 18:46:29', '2025-08-24 18:50:31');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `instructores`
---
-
-CREATE TABLE `instructores` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `documento` varchar(255) NOT NULL,
-  `telefono` varchar(255) DEFAULT NULL,
-  `email` varchar(255) NOT NULL,
-  `nombres` varchar(255) NOT NULL,
-  `apellidos` varchar(255) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Volcado de datos para la tabla `instructores`
---
-
-INSERT INTO `instructores` (`id`, `documento`, `telefono`, `email`, `nombres`, `apellidos`, `created_at`, `updated_at`) VALUES
-(1, '20500500', '300123456', 'dario@email.com', 'Darío', 'Peraz', '2025-08-24 01:26:10', '2025-08-24 01:26:10'),
-(2, '200200100', '130789456', 'carlos@email.com', 'Carlos Andres', 'Romero Romero', '2025-08-24 01:33:29', '2025-08-24 01:34:07'),
-(3, '80500200', '3115006000', 'julio@email.com', 'julio andres', 'torres flores', '2025-08-24 06:43:10', '2025-08-24 06:43:10');
+INSERT INTO `horarios` (`id`, `dia`, `fecha`, `hora_inicio`, `hora_fin`, `instructor_id`, `grupo_id`, `created_at`, `updated_at`) VALUES
+(1, 'lunes', '2025-08-26', '12:33:00', '15:36:00', 3, 2, '2025-08-26 09:33:07', '2025-08-26 09:33:07');
 
 -- --------------------------------------------------------
 
@@ -246,24 +194,12 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '0001_01_01_000002_create_jobs_table', 1),
 (4, '2025_06_19_181702_create_products_table', 1),
 (5, '2025_06_19_184223_create_estudiantes_table', 1),
-(6, '2025_06_23_205042_create_instructores_table', 1),
-(7, '2025_06_23_205251_create_grupos_table', 1),
-(8, '2025_06_23_205802_create_horarios_table', 1),
-(9, '2025_06_29_163749_add_timestamps_to_estudiantes_table', 1),
-(10, '2025_06_30_202209_create_permission_tables', 1),
-(11, '2025_08_03_004144_create_pagos_table', 1),
-(12, '2025_08_11_210559_add_concepto_to_pagos_table', 1),
-(13, '2025_08_18_015600_create_contactos_table', 2),
-(14, '2025_08_18_020122_create_contactos_table', 3),
-(15, '2025_08_18_142535_remove_mes_anio_from_pagos_table', 4),
-(16, '2025_08_18_153509_add_unique_pagos', 5),
-(17, '2025_08_18_153850_add_unique_estudiante_tipo_to_pagos_table', 5),
-(18, '2025_08_23_190210_add_documento_email_to_instructores', 6),
-(19, '2025_08_23_191400_rename_nombre_add_apellidos_to_instructores', 7),
-(20, '2025_08_23_200014_add_telefono_to_instructores_table', 8),
-(21, '2025_08_24_013052_rename_dia_to_fecha_in_horarios_table', 9),
-(22, '2025_07_17_172711_create_subgrupos_table', 10),
-(23, '2025_08_15_023001_add_deleted_at_to_users_table--table=users', 10);
+(6, '2025_06_23_205251_create_grupos_table', 1),
+(7, '2025_06_23_205802_create_horarios_table', 1),
+(8, '2025_06_29_163749_add_timestamps_to_estudiantes_table', 1),
+(9, '2025_06_30_202209_create_permission_tables', 1),
+(10, '2025_07_17_172711_create_subgrupos_table', 1),
+(11, '2025_08_15_023001_add_deleted_at_to_users_table--table=users', 1);
 
 -- --------------------------------------------------------
 
@@ -295,40 +231,10 @@ CREATE TABLE `model_has_roles` (
 
 INSERT INTO `model_has_roles` (`role_id`, `model_type`, `model_id`) VALUES
 (1, 'App\\Models\\User', 1),
-(2, 'App\\Models\\User', 3),
-(3, 'App\\Models\\User', 2),
-(3, 'App\\Models\\User', 4);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `pagos`
---
-
-CREATE TABLE `pagos` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `concepto` varchar(255) DEFAULT NULL,
-  `tipo` varchar(255) NOT NULL,
-  `valor` decimal(10,2) NOT NULL,
-  `fecha_pago` date NOT NULL,
-  `estado` varchar(255) NOT NULL,
-  `estudiante_documento` bigint(20) UNSIGNED NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `medio_pago` enum('efectivo','nequi','daviplata','transferencia') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Volcado de datos para la tabla `pagos`
---
-
-INSERT INTO `pagos` (`id`, `concepto`, `tipo`, `valor`, `fecha_pago`, `estado`, `estudiante_documento`, `created_at`, `updated_at`, `medio_pago`) VALUES
-(1, 'mes de septiembre', 'mensualidad', 400000.00, '2025-08-18', 'Pagado', 10000, '2025-08-18 19:34:25', '2025-08-23 21:01:50', 'transferencia'),
-(4, NULL, 'inscripción', 900000.00, '2025-08-01', 'Pagado', 10000, '2025-08-18 20:02:15', '2025-08-18 20:02:15', 'transferencia'),
-(8, NULL, 'inscripción', 220000.00, '2025-08-22', 'Pagado', 700700700, '2025-08-23 21:15:11', '2025-08-23 21:17:47', 'transferencia'),
-(10, 'pago mes septiembre', 'mensualidad', 230000.00, '2025-08-23', 'Pagado', 700700700, '2025-08-23 21:16:59', '2025-08-23 21:16:59', 'efectivo'),
-(11, NULL, 'inscripción', 650000.00, '2025-08-24', 'Pagado', 500600700, '2025-08-24 19:39:23', '2025-08-24 19:39:23', 'daviplata'),
-(12, 'pago agosto', 'mensualidad', 210000.00, '2025-08-24', 'Pagado', 500600700, '2025-08-24 19:40:15', '2025-08-24 19:40:15', 'transferencia');
+(2, 'App\\Models\\User', 2),
+(3, 'App\\Models\\User', 3),
+(3, 'App\\Models\\User', 4),
+(3, 'App\\Models\\User', 5);
 
 -- --------------------------------------------------------
 
@@ -375,9 +281,9 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `guard_name`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'web', '2025-08-25 18:13:37', '2025-08-25 18:13:37'),
-(2, 'colaborador', 'web', '2025-08-25 18:13:49', '2025-08-25 18:13:49'),
-(3, 'instructor', 'web', '2025-08-25 18:14:00', '2025-08-25 18:14:00');
+(1, 'admin', 'web', '2025-08-26 09:12:17', '2025-08-26 09:12:17'),
+(2, 'colaborador', 'web', '2025-08-26 09:12:27', '2025-08-26 09:12:27'),
+(3, 'instructor', 'web', '2025-08-26 09:12:36', '2025-08-26 09:12:36');
 
 -- --------------------------------------------------------
 
@@ -410,8 +316,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('A1xMQsQBTNNQsgWyOS97INXX1vaBjpxzlIFXGuhZ', NULL, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiSkdSNm1JeVhid21XdTA1VzB6T2pMdTRiam5VZEtBajNGV0tQTmppNSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MTEyOiJodHRwOi8vbG9jYWxob3N0L3Nwb3J0em9uZV91bHRpbWF0ZV8xL3B1YmxpYy9yZXBvcnRlcy9pbnNjcmlwY2lvbmVzP2ZlY2hhX2Zpbj0yMDI1LTA4LTMxJmZlY2hhX2luaWNpbz0yMDI1LTA4LTAxIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319fQ==', 1756046504),
-('Tdg7px2lbD5ACQdz6ShVb6d71tk5FyELYT4QxYV5', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 OPR/120.0.0.0 (Edition std-1)', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiMTRkeUc2dnJLZWx3OWhwWmFZNzNkd3BMbnpzV1ZqamFlOHcwdndmTyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjk6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9wcm9maWxlIjt9czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTtzOjQ6ImF1dGgiO2E6MTp7czoyMToicGFzc3dvcmRfY29uZmlybWVkX2F0IjtpOjE3NTYxMjg2NzI7fX0=', 1756129498);
+('haJaQCYxP5jqZiBlgUelCYLq29x7MeluPZZvN4GH', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 OPR/120.0.0.0 (Edition std-1)', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoib21qS2U0M2hTYm1RSXJHUVVRd3ZqVHp2T2pRb0xLVDVvZk9mdUxEdyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9yZWdpc3RlciI7fX0=', 1756183830);
 
 -- --------------------------------------------------------
 
@@ -458,7 +363,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `eps`, `documento_identidad`, `foto_documento`, `direccion_hogar`, `fecha_nacimiento`, `telefono`, `foto_perfil`, `logo_personalizado`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'alex', 'hola@email.com', NULL, '$2y$12$nYlts78jo4zfTTIjpl1R/OJQdPwk1N7dYpqQ5uRyRR4U9oiDynAee', 'Coomeva', '1073599572', NULL, 'sgasdg', '2011-05-09', '3242434312', NULL, NULL, NULL, '2025-08-25 17:56:13', '2025-08-25 18:44:54', NULL);
+(1, 'alex', 'hola@email.com', NULL, '$2y$12$oj.rwyaQ2sjHYm1v9.mvYufa0HLuwF.RLJebObuCGQkW6nTAvEkNi', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2025-08-26 09:16:13', '2025-08-26 09:16:13', NULL),
+(2, 'ricardo pinzon', 'richi@email.com', NULL, '$2y$12$KzIUdKlX9yNONUhbDx6IVuOvR/fXeyVtr3HZimh2tyGw9/TJmJZ/K', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2025-08-26 09:16:59', '2025-08-26 09:16:59', NULL),
+(3, 'diego mora', 'diego@email.com', NULL, '$2y$12$kwv8W9iNQfKLnn5tTH2UPOAbevdtaGmZw5uCT8PjqM1CeUv.G9fDW', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2025-08-26 09:17:36', '2025-08-26 09:17:36', NULL),
+(4, 'leidi', 'leidi@email.com', NULL, '$2y$12$RxSYsy2YUXU87bz15FcY3eHU1u9UtDNb.aQPn6LqYmnZiRk2v2t5y', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2025-08-26 09:18:02', '2025-08-26 09:18:02', NULL),
+(5, 'alex1', 'alex@email.com', NULL, '$2y$12$ZPfOyfWsoVDwyj9AQxZgku1aYZQ2vf.rDxgS5U2r2oBclUm3XCxMi', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2025-08-26 09:18:38', '2025-08-26 09:18:38', NULL);
 
 -- --------------------------------------------------------
 
@@ -492,12 +401,6 @@ ALTER TABLE `cache_locks`
   ADD PRIMARY KEY (`key`);
 
 --
--- Indices de la tabla `contactos`
---
-ALTER TABLE `contactos`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indices de la tabla `estudiantes`
 --
 ALTER TABLE `estudiantes`
@@ -523,14 +426,6 @@ ALTER TABLE `horarios`
   ADD PRIMARY KEY (`id`),
   ADD KEY `horarios_instructor_id_foreign` (`instructor_id`),
   ADD KEY `horarios_grupo_id_foreign` (`grupo_id`);
-
---
--- Indices de la tabla `instructores`
---
-ALTER TABLE `instructores`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `instructores_documento_unique` (`documento`),
-  ADD UNIQUE KEY `instructores_email_unique` (`email`);
 
 --
 -- Indices de la tabla `jobs`
@@ -564,14 +459,6 @@ ALTER TABLE `model_has_permissions`
 ALTER TABLE `model_has_roles`
   ADD PRIMARY KEY (`role_id`,`model_id`,`model_type`),
   ADD KEY `model_has_roles_model_id_model_type_index` (`model_id`,`model_type`);
-
---
--- Indices de la tabla `pagos`
---
-ALTER TABLE `pagos`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `pagos_estudiante_documento_tipo_unique` (`estudiante_documento`,`tipo`),
-  ADD UNIQUE KEY `unique_pago_estudiante_tipo` (`estudiante_documento`,`tipo`);
 
 --
 -- Indices de la tabla `password_reset_tokens`
@@ -633,12 +520,6 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `contactos`
---
-ALTER TABLE `contactos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT de la tabla `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -654,13 +535,7 @@ ALTER TABLE `grupos`
 -- AUTO_INCREMENT de la tabla `horarios`
 --
 ALTER TABLE `horarios`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de la tabla `instructores`
---
-ALTER TABLE `instructores`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `jobs`
@@ -672,13 +547,7 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
-
---
--- AUTO_INCREMENT de la tabla `pagos`
---
-ALTER TABLE `pagos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `permissions`
@@ -702,7 +571,7 @@ ALTER TABLE `subgrupos`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -719,7 +588,7 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `horarios`
   ADD CONSTRAINT `horarios_grupo_id_foreign` FOREIGN KEY (`grupo_id`) REFERENCES `grupos` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `horarios_instructor_id_foreign` FOREIGN KEY (`instructor_id`) REFERENCES `instructores` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `horarios_instructor_id_foreign` FOREIGN KEY (`instructor_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Filtros para la tabla `model_has_permissions`
@@ -732,12 +601,6 @@ ALTER TABLE `model_has_permissions`
 --
 ALTER TABLE `model_has_roles`
   ADD CONSTRAINT `model_has_roles_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
-
---
--- Filtros para la tabla `pagos`
---
-ALTER TABLE `pagos`
-  ADD CONSTRAINT `pagos_estudiante_documento_foreign` FOREIGN KEY (`estudiante_documento`) REFERENCES `estudiantes` (`documento`) ON DELETE CASCADE;
 
 --
 -- Filtros para la tabla `role_has_permissions`
