c0b08199ff5dbf124d55c8c0bff642297a46166e
alex sanchez <alexsanchez.az2005@gmail.com>
2025-08-25 23:50:38 -0500

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,16 +5,11 @@
     <title>Title</title>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <!-- Bootstrap CSS v5.2.1 -->
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-        crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link rel="stylesheet" href="{{ asset('assets/estilos_login.css') }}">
 </head>
 
@@ -28,7 +23,7 @@
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-xl-10">
-                    <div class="card rounded-3 text-black">
+                    <div class="card rounded-3 text-black animated-card">
                         <div class="row g-0">
                             <div class="col-lg-6">
                                 <div class="card-body p-md-5 mx-md-4">
@@ -66,7 +61,8 @@
                                         </div>
 
                                         <div data-mdb-input-init class="form-outline mb-4">
-                                            <input type="password" name="password_confirmation" id="password_confirmation" class="form-control"
+                                            <input type="password" name="password_confirmation"
+                                                id="password_confirmation" class="form-control"
                                                 placeholder="Digita la contraseña" />
                                             <label class="form-label" for="password">Confirmar Contraseña</label>
                                         </div>
@@ -81,7 +77,8 @@
 
                                         <div class="d-flex align-items-center justify-content-center pb-4">
                                             <p class="mb-0 me-2">Ir al Login</p>
-                                            <a href="{{ route('login') }}" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-danger">Login</a>
+                                            <a href="{{ route('login') }}" data-mdb-button-init data-mdb-ripple-init
+                                                class="btn btn-outline-danger">Login</a>
                                         </div>
 
 
@@ -93,9 +90,11 @@
                             <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
                                 <div class="text-white px-3 py-4 p-md-5 mx-md-4">
                                     <h4 class="mt-1 mb-5 pb-1">¡Bienvenidos!</h4>
-                                    <p class="small mb-0">Nos alegra tenerte aquí. Inicia sesión para acceder a tu panel personalizado, 
-                                        gestionar tus actividades y mantenerte conectado con tu comunidad deportiva. Ya seas entrenador,
-                                         administrador o colaborador.</p>
+                                    <p class="small mb-0">Nos alegra tenerte aquí. Inicia sesión para acceder a tu panel
+                                        personalizado,
+                                        gestionar tus actividades y mantenerte conectado con tu comunidad deportiva. Ya
+                                        seas entrenador,
+                                        administrador o colaborador.</p>
                                 </div>
                             </div>
                         </div>
@@ -107,13 +106,11 @@
 
 
     <!-- Bootstrap JavaScript Libraries -->
-    <script
-        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
         crossorigin="anonymous"></script>
 
-    <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
         integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
         crossorigin="anonymous"></script>
 </body>
